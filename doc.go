12e333ea97a25c0395c3e9dba5d04cc681cead9d
johnbass <johnbass1017@yahoo.com>
2024-12-26 15:07:58 -0800

    chore: reuse

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 /*
 Package medley implements distributed hashing aimed at microservices.
 Currently, only consistent hashing is implemented.
